f749f5b6ff602c2dcb4feb0cc64db85c84d4bb15
kozzztia <kozzztia@gmail.com>
2024-09-08 14:12:37 +0300

    create rundom animation with cht forget that js create element and its dont anumate them

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-import {deckOfCards} from "./cards.js";
+import { deckOfCards } from "./cards.js";
 const deck = document.querySelector('#deck');
 
 
-console.log(deckOfCards)
-
 function renderCards() {
-    deck.innerHTML = "";
-    deckOfCards.map((item) => {
-        const card = document.createElement('div');
-        card.classList = "card";
-                // Обробник події кліку
-                card.addEventListener('click', () => {
-                    item.isOpen = !item.isOpen;
-                    renderCards();  // Перерендерити всі карти після зміни стану
-                });
-        
-
-        card.innerHTML = `
-                <div class="card-inner ${item.isOpen?"active":null}">
-                    <div class="card-front">
-                        <svg>
-                            <use xlink:href="./assets/cards.svg#${item.name}"></use>
-                        </svg>
-                    </div>
-                    <div class="card-back">
-                        <img src="./assets/card-back-green.png" alt="back">
-                    </div>
-                </div>
-        `;
-        deck.appendChild(card)
-    })
+    deckOfCards.forEach((item, index) => {
+        const card = document.querySelectorAll('.card')[index];
+        const cardInner = card.querySelector('.card-inner');
+        if (item.isOpen) {
+            cardInner.classList.add('active');
+        } else {
+            cardInner.classList.remove('active');
+        }
+    });
 }
 
-renderCards()
+function initCards() {
+    deck.innerHTML = "";
+    deckOfCards.forEach((item) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <div class="card-inner">
+                <div class="card-front">
+                    <svg>
+                        <use xlink:href="./assets/cards.svg#${item.name}"></use>
+                    </svg>
+                </div>
+                <div class="card-back">
+                    <img src="./assets/card-back-green.png" alt="back">
+                </div>
+            </div>
+        `;
+        deck.appendChild(card);
+
+        card.addEventListener('click', () => {
+            item.isOpen = !item.isOpen;
+            renderCards(); 
+        });
+    });
+}
+
+initCards();
+
+
+setInterval(() => {
+    const randomIndex = Math.floor(Math.random() * deckOfCards.length);
+    console.log(randomIndex);
+    deckOfCards.forEach(item => item.isOpen = false);
+    deckOfCards[randomIndex].isOpen = true;
+    renderCards();
+}, 2000);
